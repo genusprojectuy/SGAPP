e268fb27f273d4bdbec63ae6373002190dc954b0
alvaro051092 <alvaro051092@gmail.com>
2017-07-31 20:12:59 -0300

    Initial commit

---
+++ app/src/main/java/uy/edu/ctc/sgapp/user_interface/MainActivity.java
@@ -0,0 +1,17 @@
+package uy.edu.ctc.sgapp.user_interface;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uy.edu.ctc.sgapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
